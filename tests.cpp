8a00583413945e721898b5d0c4df78e9fbd69a6b
Sergey <wer113wer@yandex.ru>
2022-10-20 23:13:29 +0300

    Добавлены дополнительные тесты на функцию getInputID

--- tests.cpp
+++ tests.cpp
@@ -10,7 +10,7 @@ tests::tests(QObject *parent)
 
 void tests::getInputID_test_success()
 {
-    QString locationTxt = QDir::currentPath() + "..\\..\\kinpo-hierarchyy\\tests\\id_correct.txt";  // Расположение входного .txt файла
+    QString locationTxt = QDir::currentPath() + "..\\..\\kinpo-hierarchyy\\tests\\id_correct.txt";
     QFile inputTxt(locationTxt);
 
     int result = getInputID(inputTxt);
@@ -20,7 +20,7 @@ void tests::getInputID_test_success()
 
 void tests::getInputID_test_zero()
 {
-    QString locationTxt = QDir::currentPath() + "..\\..\\kinpo-hierarchyy\\tests\\id_incorrect_zero.txt";  // Расположение входного .txt файла
+    QString locationTxt = QDir::currentPath() + "..\\..\\kinpo-hierarchyy\\tests\\id_incorrect_zero.txt";
     QFile inputTxt(locationTxt);
 
     int result = getInputID(inputTxt);
@@ -30,7 +30,17 @@ void tests::getInputID_test_zero()
 
 void tests::getInputID_test_space()
 {
-    QString locationTxt = QDir::currentPath() + "..\\..\\kinpo-hierarchyy\\tests\\id_incorrect_space.txt";  // Расположение входного .txt файла
+    QString locationTxt = QDir::currentPath() + "..\\..\\kinpo-hierarchyy\\tests\\id_incorrect_space.txt";
+    QFile inputTxt(locationTxt);
+
+    int result = getInputID(inputTxt);
+
+    QVERIFY2(0 == result, "Success");
+}
+
+void tests::getInputID_test_empty()
+{
+    QString locationTxt = QDir::currentPath() + "..\\..\\kinpo-hierarchyy\\tests\\id_incorrect_empty.txt";
     QFile inputTxt(locationTxt);
 
     int result = getInputID(inputTxt);
@@ -40,7 +50,37 @@ void tests::getInputID_test_space()
 
 void tests::getInputID_test_word()
 {
-    QString locationTxt = QDir::currentPath() + "..\\..\\kinpo-hierarchyy\\tests\\id_incorrect_word.txt";  // Расположение входного .txt файла
+    QString locationTxt = QDir::currentPath() + "..\\..\\kinpo-hierarchyy\\tests\\id_incorrect_word.txt";
+    QFile inputTxt(locationTxt);
+
+    int result = getInputID(inputTxt);
+
+    QVERIFY2(0 == result, "Success");
+}
+
+void tests::getInputID_test_two_value()
+{
+    QString locationTxt = QDir::currentPath() + "..\\..\\kinpo-hierarchyy\\tests\\id_incorrect_two_value.txt";
+    QFile inputTxt(locationTxt);
+
+    int result = getInputID(inputTxt);
+
+    QVERIFY2(0 == result, "Success");
+}
+
+void tests::getInputID_test_word_and_value()
+{
+    QString locationTxt = QDir::currentPath() + "..\\..\\kinpo-hierarchyy\\tests\\id_incorrect_word_and_value.txt";
+    QFile inputTxt(locationTxt);
+
+    int result = getInputID(inputTxt);
+
+    QVERIFY2(0 == result, "Success");
+}
+
+void tests::getInputID_test_complex()
+{
+    QString locationTxt = QDir::currentPath() + "..\\..\\kinpo-hierarchyy\\tests\\id_incorrect_complex.txt";
     QFile inputTxt(locationTxt);
 
     int result = getInputID(inputTxt);
@@ -50,7 +90,7 @@ void tests::getInputID_test_word()
 
 void tests::getAllContentFromXml_success()
 {
-    QString locationXml = QDir::currentPath() + "..\\..\\kinpo-hierarchyy\\tests\\xml_correct.xml";  // Расположение входного .xml файла
+    QString locationXml = QDir::currentPath() + "..\\..\\kinpo-hierarchyy\\tests\\xml_correct.xml";
     QFile inputXml(locationXml);
 
     department rootDepartment;
@@ -61,7 +101,7 @@ void tests::getAllContentFromXml_success()
 
 void tests::getAllContentFromXml_zero()
 {
-    QString locationXml = QDir::currentPath() + "..\\..\\kinpo-hierarchyy\\tests\\xml_zero.xml";  // Расположение входного .xml файла
+    QString locationXml = QDir::currentPath() + "..\\..\\kinpo-hierarchyy\\tests\\xml_zero.xml";
     QFile inputXml(locationXml);
 
     department rootDepartment;
@@ -72,7 +112,7 @@ void tests::getAllContentFromXml_zero()
 
 void tests::getAllContentFromXml_person()
 {
-    QString locationXml = QDir::currentPath() + "..\\..\\kinpo-hierarchyy\\tests\\xml_person.xml";  // Расположение входного .xml файла
+    QString locationXml = QDir::currentPath() + "..\\..\\kinpo-hierarchyy\\tests\\xml_person.xml";
     QFile inputXml(locationXml);
 
     department rootDepartment;
@@ -80,3 +120,36 @@ void tests::getAllContentFromXml_person()
 
     QVERIFY2(0 == result, "Success");
 }
+
+//void tests::getInputXmlDatasToStructs_success()
+//{
+//    QString locationXml = QDir::currentPath() + "..\\..\\kinpo-hierarchyy\\tests\\xml_person.xml";  // Расположение входного .xml файла
+//    QFile inputXml(locationXml);
+
+//    department rootDepartment;
+//    getAllContentFromXml(inputXml, rootDepartment);
+
+//    if (!inputXml.open(QIODevice::ReadOnly))    // Проверяем, возможно ли открыть .xml файл для чтения
+//        qDebug() << "Error: can't read .xml file";  // Вывести в консоль ошибку, что файл открыть невозможно
+
+//    QByteArray buff = inputXml.readAll();
+//    QDomDocument doc;
+
+
+//    QDomElement root = doc.documentElement();
+
+//    // Первая вложенная нода
+//    QDomNode record_node = root.firstChild();
+
+//    // Список подчиненных сотрудников
+//    QList<employee> childEmployees;
+
+//    // Список подчиненных подразделений
+//    QList<department> childDepartments;
+
+//    QList<employee> expected_childEmployees({1111, "Директор", "ООО TechLab"});
+//    getInputXmlDatasToStructs(record_node, root, childEmployees, childDepartments);
+
+////    QVERIFY2(0 == result, "Success");
+//}
+
--- tests.h
+++ tests.h
@@ -13,12 +13,16 @@ private slots:
     void getInputID_test_success();
     void getInputID_test_zero();
     void getInputID_test_space();
+    void getInputID_test_empty();
     void getInputID_test_word();
+    void getInputID_test_two_value();
+    void getInputID_test_word_and_value();
+    void getInputID_test_complex();
 
     void getAllContentFromXml_success();
     void getAllContentFromXml_zero();
     void getAllContentFromXml_person();
-
+//    void getInputXmlDatasToStructs_success();
 };
 
 #endif // TESTS_H
