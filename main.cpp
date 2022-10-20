805aa380351c6a17b5fd8d2de643304f0184d8fe
Sergey <wer113wer@yandex.ru>
2022-10-20 12:39:33 +0300

    Реализованы функции testInputDatas и outputError

--- main.cpp
+++ main.cpp
@@ -11,12 +11,48 @@ int main(int argc, char *argv[])
 
 bool testInputDatas(QString& locationXml, QFile& inputXml, QString& locationTxt, QFile& inputTxt)
 {
+    // Проверка наличия входного файла .xml
+    if (!inputXml.exists())
+    {
+        //puts("error: no input .xml");
+        throw 1;
+    }
 
+    // Проверка корректности входного .xml файла
+    if (!inputXml.open(QIODevice::ReadOnly | QIODevice::Text))
+    {
+        //puts("invalid input data: unable to read .xml file");
+        throw 2;
+
+    }
+
+    // Проверка наличия входного файла .txt
+    if (!inputTxt.exists())
+    {
+        //puts("error: no input .txt");
+        throw 3;
+    }
+
+    // Проверка наличия ID во входном .txt файле
+    if(inputTxt.size() == 0)
+    {
+        //puts("invalid input data: .txt file is empty");
+        throw 4;
+    }
+
+    inputXml.close();   // закрыть .xml файл
 }
 
 void outputError(int error)
 {
-
+    if(error == 1)
+        qDebug() << "error: no input .xml";
+    if(error == 2)
+        qDebug() << "invalid input data: unable to read .xml file";
+    if(error == 3)
+        qDebug() << "error: no input .txt";
+    if(error == 4)
+        qDebug() << "invalid input data: .txt file is empty";
 }
 
 void getInputDatas()
